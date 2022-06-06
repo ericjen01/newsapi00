5e78dc2d659245933ff3a5cac94643df2cf51e9a
ericjen01 <eric.jen01@gmail.com>
2022-06-06 04:55:36 -0700

    layout and grid styling added

--- assets/main.js
+++ assets/main.js
@@ -4,40 +4,29 @@ const newsList = document.querySelector(".newsList");
 fetch(url)
 	.then((res) => res.json())
 	.then((data) => {
+		data.shift(); // delete the first article in the data array as it never follows the proper format
+		console.log(data);
 		data.forEach((article) => {
-			console.log(article.title);
+			let title = article.title;
+			let date = article.time;
+			let source = article.source;
+			let img = article.image;
+			let url = article.url;
+			if (url.startsWith("/life")) {
+				url = "https://www.cbc.ca" + url;
+			}
+
+			let li = document.createElement("li");
+			li.classList.add("plainList");
+			li.innerHTML = `
+        <a class="card plainText" href='${url}' target="_blank">
+          <img class="newsImg" src="${img}" alt="">
+          <div class="card_content">
+		  	<h1 class="date">${date} </h1>
+            <h1 class="title">${title}</h1>
+          </div>
+        </a>
+      `;
+			newsList.appendChild(li);
 		});
 	});
-
-/*
-
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4) {
-    console.log(xhr.status);
-    let data = JSON.parse(xhr.responseText);
-    console.log(data.articles);
-    data.articles.forEach((article) => {
-      console.log(article.link);
-      let li = document.createElement("li");
-      li.innerHTML = `
-      <a href="${article.link}" target="_blank">
-        <img class="newspic" src="${article.media}" alt="">
-      </a>
-      <div>
-        <a href="${article.link}" target="_blank">
-          <h1>${article.title}</h1>
-        </a>
-        <div>
-          <p class="articleDate">${article.published_date}</p>
-          <p class="articleSummary">${article.summary}</p>
-        </div>
-      </div>
-      `;
-      newsList.appendChild(li);
-    });
-  }
-};
-
-xhr.send();
-
-*/
